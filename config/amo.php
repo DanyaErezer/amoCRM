265813af27fc116f423ed2d2ee4720364292837b
DanyaErezer <pavlov.shura12@gmail.com>
2025-04-09 18:50:27 +0700

    Feature: переброс env данных

--- config/amo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'client_id' => '9421844c-5aef-4bcb-a534-c639aa4e8dec',
-    'client_secret' => 'rRRWg4NKYeFsoPzk39xkE2auYk5sQAIsOjc90f9TU4duOTmWx8650CUiGmOiiuSn',
-    'redirect_uri' => 'https://1234abcd.ngrok.io/amo-auth/callback',
-    'subdomain' => 'pavlovshura12',
-];
